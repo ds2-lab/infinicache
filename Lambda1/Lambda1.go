ee2584a1f12738749cee9139b5e6d583afdcc09f
Ao Wang <awang24@gmu.edu>
2019-05-17 00:45:40 -0400

    EC2hub λ receive function

--- Lambda1/Lambda1.go
+++ Lambda1/Lambda1.go
@@ -9,25 +9,34 @@ import (
 	"time"
 )
 
-type req struct {
+type header struct {
 	Name      string
 	TimeStamp time.Time
 	Uuid      [16]byte
 }
 
-type response struct {
-	Length int
-	Obj    []byte
-	Uuid   [16]byte
+type body struct {
+	obj []byte
 }
 
-func encode(name string) []byte {
-	myReq := &req{
+func encodeHeader(name string) []byte {
+	myHeader := &header{
 		Name:      name,
 		TimeStamp: time.Now(),
 		Uuid:      uuid.New(),
 	}
-	encoded, err := binary.Marshal(myReq)
+	encoded, err := binary.Marshal(myHeader)
+	if err != nil {
+		fmt.Println("encode err", err)
+	}
+	return encoded
+}
+
+func encodeBody() []byte {
+	myBody := &body{
+		obj: []byte{1, 2, 3},
+	}
+	encoded, err := binary.Marshal(myBody)
 	if err != nil {
 		fmt.Println("encode err", err)
 	}
@@ -49,20 +58,62 @@ func connect() *net.TCPConn {
 	return tcpConn
 }
 
+func tcpSend(conn *net.TCPConn, headerLen []byte, bodyLen []byte, header []byte, body []byte) {
+	fmt.Println(headerLen)
+	fmt.Println(bodyLen)
+	fmt.Println(header)
+	fmt.Println(body)
+
+	sendCount := 0
+	switch sendCount {
+	case 0:
+		_, err := conn.Write(headerLen)
+		if err != nil {
+			fmt.Println(err)
+		}
+		sendCount = 1
+		fallthrough
+	case 1:
+		_, err := conn.Write(bodyLen)
+		if err != nil {
+			fmt.Println(err)
+		}
+		sendCount = 2
+		fallthrough
+	case 2:
+		_, err := conn.Write(header)
+		if err != nil {
+			fmt.Println(err)
+		}
+		sendCount = 3
+		fallthrough
+	case 4:
+		_, err := conn.Write(body)
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+}
+
 func main() {
 	tcpCoon := connect()
 	defer tcpCoon.Close() //close conn
 
-	// send request to server
-	encodedReq := encode("Lambda2SmallJPG")
-	n, err := tcpCoon.Write(encodedReq) // send data
-	if err != nil {
-		fmt.Println(err)
-	}
-	fmt.Println("Send", n, "byte data successed", "message is\n", encodedReq)
+	myHeader := encodeHeader("Lambda2SmallJPG")
+	myBody := encodeBody()
+
+	myHeaderLen := make([]byte, 4)
+	myBodyLen := make([]byte, 4)
+
+	binary.BigEndian.PutUint32(myHeaderLen, uint32(len(myHeader))) // get header length
+	binary.BigEndian.PutUint32(myBodyLen, uint32(len(myBody)))     // get body length
+
+	tcpSend(tcpCoon, myHeaderLen, myBodyLen, myHeader, myBody)
 
 	// receive from server
 	var b bytes.Buffer
+	var info header
 	//buff := make([]byte, 2048)
 	//bs := make([]byte, 4)
 	//n, err = tcpCoon.Read(bs)
@@ -73,26 +124,32 @@ func main() {
 	//s := binary.LittleEndian.Uint32(bs[:n])
 	//fmt.Printf("s is %d\n", int(s))
 	//length := 0
-	infoBuff := make([]byte, 2048)
-	fileBuff := make([]byte, 2048)
-	n, err = tcpCoon.Read(infoBuff)
+	infoBuff := make([]byte, 4)
+	bodyLenBuff := make([]byte, 4)
+	bodyBuff := make([]byte, 2048)
+
+	n, err := tcpCoon.Read(infoBuff) // conn read header
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
-	var info response
-	count := 0
 	_ = binary.Unmarshal(infoBuff[:n], &info)
-	length := info.Length
-	fmt.Println("total size:", length)
+
+	n, err = tcpCoon.Read(bodyLenBuff) // conn read BodyLen
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	count := 0
+	length := n
 
 	for {
-		n, err := tcpCoon.Read(fileBuff)
+		n, err := tcpCoon.Read(bodyBuff)
 		//fmt.Println(n)
 		count += n
 		fmt.Println("length", count)
 		if n > 0 {
-			b.Write(fileBuff[:n])
+			b.Write(bodyBuff[:n])
 		}
 		if count >= length {
 			break
--- ec2_server/EC2_Hub.go
+++ ec2_server/EC2_Hub.go
@@ -17,24 +17,33 @@ type lambdaInstance struct {
 	alive bool
 }
 
-type req struct {
-	Name      string
-	TimeStamp time.Time
-	Uuid      [16]byte
+type header struct {
+	name      string
+	timeStamp time.Time
+	uuid      [16]byte
 }
 
+type Frame struct {
+	lenHeader uint32
+	lenBody   uint32
+	header    []byte
+	body      []byte
+}
+
+var myLambda lambdaInstance
 var lambda1 lambdaInstance
 var lambda2 *lambdaInstance
+var frameHandler chan *Frame
 
 func newLambdaInstance(name string) *lambdaInstance {
 	return &lambdaInstance{name: name}
 }
 
 // server decode request from lambda1
-func decode() req {
+func decode() header {
 	reqBuff := make([]byte, 2048)
 	n, err := lambda1.conn.Read(reqBuff)
-	var v req
+	var v header
 	err = binary.Unmarshal(reqBuff[:n], &v)
 	if err != nil {
 		fmt.Println("decode err is ", err)
@@ -56,41 +65,132 @@ func main() {
 	}
 	fmt.Println("Start listening", tcpAddr, "and waiting for client")
 
+	//for {
+	//	lambda1.conn, err = tcpListener.AcceptTCP() //Accept lambda1
+	//	if err != nil {
+	//		fmt.Println(err)
+	//		return
+	//	}
+	//	fmt.Println("lambda1 Connect :", lambda1.conn.RemoteAddr())
+	//
+	//	// get request struct
+	//	//destination := decode()
+	//	fmt.Println("destination is ", "Lambda2SmallJPG")
+	//
+	//	if lambda2 == nil {
+	//		lambda2 = newLambdaInstance("Lambda2SmallJPG")
+	//	}
+	//	if !lambda2.alive {
+	//		fmt.Println("Lambda 2 is sleeping, need to activate")
+	//		go lambdaTrigger(lambda2) // trigger lambda
+	//		fmt.Println("Lambda 2 is active now")
+	//		time.Sleep(2 * time.Second)
+	//	}
+	//	if lambda2.conn == nil {
+	//		lambda2.conn, err = tcpListener.AcceptTCP() //Accept lambda2_simulator
+	//		if err != nil {
+	//			fmt.Println(err)
+	//			return
+	//		}
+	//		fmt.Println("Lamdba2 Connect :", lambda2.conn.RemoteAddr())
+	//	}
+	//	go receive1(&lambda1, lambda2)
+	//	go receive2(lambda2, &lambda1)
+	//}
 	for {
-		lambda1.conn, err = tcpListener.AcceptTCP() //Accept lambda1
+		myLambda.conn, err = tcpListener.AcceptTCP()
 		if err != nil {
 			fmt.Println(err)
 			return
 		}
-		fmt.Println("lambda1 Connect :", lambda1.conn.RemoteAddr())
-		// get request struct
-		//destination := decode()
-		fmt.Println("destination is ", "Lambda2SmallJPG")
+		fmt.Println("lambda connect: ", myLambda.conn.RemoteAddr())
 
-		if lambda2 == nil {
-			lambda2 = newLambdaInstance("Lambda2SmallJPG")
-		}
-		if !lambda2.alive {
-			fmt.Println("Lambda 2 is sleeping, need to activate")
-			go lambdaTrigger(lambda2) // trigger lambda
-			fmt.Println("Lambda 2 is active now")
-			time.Sleep(2 * time.Second)
-		}
-		if lambda2.conn == nil {
-			lambda2.conn, err = tcpListener.AcceptTCP() //Accept lambda2_simulator
-			if err != nil {
-				fmt.Println(err)
-				return
-			}
-			fmt.Println("Lamdba2 Connect :", lambda2.conn.RemoteAddr())
-		}
-		go receive1(&lambda1, lambda2)
-		go receive2(lambda2, &lambda1)
+		receiveLambda(myLambda)
 
 	}
-
 }
 
+func receiveLambda(l lambdaInstance) {
+	time.Sleep(1 * time.Second)
+	defer l.conn.Close()
+	frame := &Frame{
+		lenHeader: 0,
+		lenBody:   0,
+		header:    []byte{},
+		body:      []byte{},
+	}
+	intBuff := make([]byte, 4)
+	fieldFrame := 0
+
+	buff := make([]byte, 2048)
+	for {
+		buffFrom := 0
+		n, err := l.conn.Read(buff)
+		if err == io.EOF {
+			fmt.Println(l.conn.RemoteAddr(), "has disconnect")
+			break
+		} else if err != nil {
+			fmt.Println(l.conn.RemoteAddr(), "connection error")
+			break
+		}
+		fmt.Println("receive lambda's message is: ", buff[:n])
+
+		switch fieldFrame {
+		case 0:
+			// get header length
+			if n-buffFrom >= cap(intBuff) {
+				frame.lenHeader = binary.BigEndian.Uint32(buff[buffFrom : buffFrom+4])
+				buffFrom = buffFrom + 4
+				fieldFrame = 1
+				goto nextCase0
+			} else {
+				copy(intBuff, buff[buffFrom:n])
+			}
+		nextCase0:
+			fmt.Println("case0")
+			fallthrough
+		case 1:
+			// get body length
+			if n-buffFrom >= cap(intBuff) {
+				frame.lenBody = binary.BigEndian.Uint32(buff[buffFrom : buffFrom+4])
+				buffFrom = buffFrom + 4
+				fieldFrame = 2
+				goto nextCase1
+			} else {
+				copy(intBuff, buff[buffFrom:n])
+			}
+		nextCase1:
+			fmt.Println("case1")
+			fallthrough
+		case 2:
+			// get header
+			if n-buffFrom >= int(frame.lenHeader)-len(frame.header) {
+				frame.header = merge(frame.header, buff[buffFrom:buffFrom+int(frame.lenHeader)])
+				fieldFrame = 3
+				buffFrom = buffFrom + int(frame.lenHeader) - len(frame.header)
+				goto nextCase2
+			} else {
+				frame.header = merge(frame.header, buff[buffFrom:])
+			}
+		nextCase2:
+			fmt.Println("case2")
+
+			fallthrough
+		case 3:
+			// get body
+			if n-buffFrom >= int(frame.lenBody)-len(frame.body) {
+				frame.body = merge(frame.body, buff[buffFrom:buffFrom+int(frame.lenBody)])
+				buffFrom = buffFrom + int(frame.lenBody) - len(frame.body)
+				//frameHandler <- frame
+				//frame = &Frame{}
+				fieldFrame = 0
+			} else {
+				frame.body = merge(frame.body, buff[buffFrom:])
+			}
+		}
+		fmt.Println("frame is", frame)
+	}
+}
 func receive1(l1 *lambdaInstance, l2 *lambdaInstance) {
 
 	defer l1.conn.Close()
@@ -164,5 +264,13 @@ func lambdaTrigger(instance *lambdaInstance) {
 	if err != nil {
 		fmt.Println("Error calling LambdaFunction")
 	}
+	fmt.Println("lamdba deactive")
 	instance.alive = false
+}
+
+func merge(a []byte, b []byte) []byte {
+	c := make([]byte, len(a)+len(b))
+	copy(c, a)
+	copy(c[len(a):], b)
+	return c
 }
