ca09a63b4e89da697c4e40ab391b005429b5ac2e
Tianium <zhangjyr@gmail.com>
2019-07-31 00:14:50 -0400

    Rollback/Merge some changes

--- src/LambdaStore/redeo_lambda.go
+++ src/LambdaStore/redeo_lambda.go
@@ -13,27 +13,26 @@ import (
 )
 
 type Chunk struct {
-	id   int64
+	id   string
 	body []byte
 }
 
 type DataEntry struct {
-	op              int
-	status          int
+	op              string
+	status          string
 	reqId           string
-	chunkId         int64
+	chunkId         string
 	durationAppend  time.Duration
 	durationFlush   time.Duration
 	duration        time.Duration
 }
 
-const OP_GET = 0
-const OP_SET = 1
+const OP_GET = "0"
+const OP_SET = "1"
 
 var (
-	//lambdaConn, _ = net.Dial("tcp", "54.204.180.34:6379") // 10Gbps ec2 server Proxy0
+	lambdaConn, _ = net.Dial("tcp", "3.220.244.122:6379") // 10Gbps ec2 server UbuntuProxy0
 	//lambdaConn, _ = net.Dial("tcp", "172.31.18.174:6379") // 10Gbps ec2 server Proxy1
-	lambdaConn, _ = net.Dial("tcp", "54.211.243.58:6379") // 10Gbps ec2 server Proxy0
 	srv           = redeo.NewServer(nil)
 	myMap         = make(map[string]*Chunk)
 	isFirst       = true
@@ -41,6 +40,7 @@ var (
 )
 
 func HandleRequest() {
+	timeOut := time.Duration(120 * time.Second)
 	done := make(chan struct{})
 	dataGatherer := make(chan *DataEntry, 10)
 	dataDepository := make([]*DataEntry, 0, 100)
@@ -52,11 +52,10 @@ func HandleRequest() {
 			// Define handlers
 			srv.HandleFunc("get", func(w resp.ResponseWriter, c *resp.Command) {
 				t := time.Now()
-				log.Debug("in the get function")
+				log.Debug("In GET handler")
 
-				connId, _ := c.Arg(0).Int()
+				connId := c.Arg(0).String()
 				reqId := c.Arg(1).String()
-				log.Debug("reqId is", reqId)
 				key := c.Arg(3).String()
 
 				//val, err := myCache.Get(key)
@@ -66,64 +65,63 @@ func HandleRequest() {
 				chunk, found := myMap[key]
 				if found == false {
 					log.Debug("%s not found", key)
-					dataGatherer <- &DataEntry{ OP_GET, 404, reqId, -1, 0, 0, time.Since(t) }
+					dataGatherer <- &DataEntry{ OP_GET, "404", reqId, "-1", 0, 0, time.Since(t) }
 					return
 				}
-				log.Debug("%s found, len: %d", key, len(chunk.body))
 
 				// construct lambda store response
-				w.AppendInt(connId)
+				w.AppendBulkString(connId)
 				w.AppendBulkString(reqId)
-				w.AppendInt(chunk.id)
+				w.AppendBulkString(chunk.id)
 				t2 := time.Now()
 				w.AppendBulk(chunk.body)
 				d2 := time.Since(t2)
-				log.Debug("appendBody time is ", d2)
 
 				t3 := time.Now()
 				if err := w.Flush(); err != nil {
 					log.Error("Error on get::flush(key %s): %v", key, err)
-					dataGatherer <- &DataEntry{ OP_GET, 500, reqId, chunk.id, d2, 0, time.Since(t) }
+					dataGatherer <- &DataEntry{ OP_GET, "500", reqId, chunk.id, d2, 0, time.Since(t) }
 					return
 				}
 				d3 := time.Since(t3)
-				log.Debug("flush time is ", d3)
 
 				dt := time.Since(t)
-				log.Debug("duration time is", dt)
-				log.Debug("get complete, key: %s, client id:%d, chunk id:%d", key, connId, chunk.id)
-				dataGatherer <- &DataEntry{ OP_GET, 200, reqId, chunk.id, d2, d3, dt }
+
+				log.Debug("AppendBody duration is ", d2)
+				log.Debug("Flush duration is ", d3)
+				log.Debug("Total duration is", dt)
+				log.Debug("Get complete, Key: %s, ConnID:%s, ChunkID:%s", key, connId, chunk.id)
+				dataGatherer <- &DataEntry{ OP_GET, "200", reqId, chunk.id, d2, d3, dt }
 			})
 
 			srv.HandleFunc("set", func(w resp.ResponseWriter, c *resp.Command) {
 				t := time.Now()
-				log.Debug("in the set function")
+				log.Debug("In SET handler")
 				//if c.ArgN() != 3 {
 				//	w.AppendError(redeo.WrongNumberOfArgs(c.Name))
 				//	return
 				//}
 
-				connId, _ := c.Arg(0).Int()
+				connId := c.Arg(0).String()
 				reqId := c.Arg(1).String()
-				log.Debug("reqId is ", reqId)
-				chunkId, _ := c.Arg(2).Int()
+				chunkId := c.Arg(2).String()
 				key := c.Arg(3).String()
 				val := c.Arg(4).Bytes()
 				myMap[key] = &Chunk{ chunkId, val }
 
 				// write Key, clientId, chunkId, body back to server
-				w.AppendInt(connId)
+				w.AppendBulkString(connId)
 				w.AppendBulkString(reqId)
-				w.AppendInt(chunkId)
+				w.AppendBulkString(chunkId)
 				w.AppendInt(1)
 				if err := w.Flush(); err != nil {
 					log.Error("Error on set::flush(key %s): %v", key, err)
-					dataGatherer <- &DataEntry{ OP_SET, 500, reqId, chunkId, 0, 0, time.Since(t) }
+					dataGatherer <- &DataEntry{ OP_SET, "500", reqId, chunkId, 0, 0, time.Since(t) }
 					return
 				}
 
-				log.Debug("set complete, key:%s, val len: %d, client id: %d, chunk id: %d", key, len(val), connId, chunkId)
-				dataGatherer <- &DataEntry{ OP_SET, 200, reqId, chunkId, 0, 0, time.Since(t) }
+				log.Debug("Set complete, Key:%s, ConnID: %s, ChunkID: %s, Item length", key, connId, chunkId, len(val))
+				dataGatherer <- &DataEntry{ OP_SET, "200", reqId, chunkId, 0, 0, time.Since(t) }
 			})
 
 			srv.HandleFunc("data", func(w resp.ResponseWriter, c *resp.Command) {
@@ -157,7 +155,7 @@ func HandleRequest() {
 	select {
 	case <-done:
 		return
-	case <-time.After(120 * time.Second):
+	case <-time.After(timeOut):
 		log.Debug("Lambda timeout, going to return function")
 		return
 	}
