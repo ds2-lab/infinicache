28ad39f8534a38e74802ed7cf168193624e11c08
Tianium <zhangjyr@gmail.com>
2019-08-01 14:38:27 -0400

    Adjust slap script

--- evaluation/slap.sh
+++ evaluation/slap.sh
@@ -6,6 +6,8 @@ fi
 
 PWD=`dirname $0`
 ENTRY=`date "+%Y%m%d%H%M"`
+ENTRY="/data/$ENTRY"
+NODE_PREFIX="Node"
 
 source $PWD/util.sh
 
@@ -69,7 +71,7 @@ function perform(){
         PRESET=$PWD/$ENTRY/No.$i"_"$DATA"_"$PARITY"_"lambda$MEM"_"$SZ"_SET"
         PREGET=$PWD/$ENTRY/No.$i"_"$DATA"_"$PARITY"_"lambda$MEM"_"$SZ"_GET"
 
-        update_lambda_timeout Node $((TIME+i*10))
+        update_lambda_timeout $NODE_PREFIX $((TIME+i*10))
         wait
         start_proxy $PREPROXY &
         while [ ! -f /tmp/pidLog.txt ]
@@ -87,13 +89,15 @@ function perform(){
         sleep 1s
 #        get
         bench $NUMBER $CON $KEYMIN $KEYMAX $SZ $DATA $PARITY 1 $PREGET
-        kill -9 `cat /tmp/pidLog.txt`
+        kill -2 `cat /tmp/pidLog.txt`
     done
 }
 
 #perform $*
 #perform
 
+MEMSET=(128 256 512 1024 1536 2048 3008)
+SZSET=(10485760 20971520 41943040 62914020 83886080 104857600)
 DS=(4 5 10 10 10)
 PS=(2 1 1 2 4)
 C=(1)
@@ -104,18 +108,20 @@ if [ "$1" != "" ]; then
 fi
 
 mkdir -p $PWD/$ENTRY
-for mem in 128 256 512 1024 1536 2048 3008
+for mem in {0..6}
 do
-    update_lambda_mem Node $mem
-    for sz in 10485760 20971520 41943040 62914020 83886080 104857600
+    update_lambda_mem $NODE_PREFIX ${MEMSET[mem]}
+    for sz in {0..5}
     do
         for k in {0..4}
         do
             for l in 0
             do
-                perform $mem ${N[l]} ${C[l]} 1 2 $sz ${DS[k]} ${PS[k]} $1
-#                perform $mem 1 3 1 2 $sz ${DS[k]} ${PS[k]} 200
+                # perform mem          num     concur keymin keymax objsize   datashards parity timeout
+                perform ${MEMSET[mem]} ${N[l]} ${C[l]} 1 ${N[l]} ${SZSET[sz]} ${DS[k]} ${PS[k]} $TIMEOUT
             done
         done
     done
 done
+
+mv $PWD/log $PWD/$ENTRY.log
