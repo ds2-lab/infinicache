eb618683013ae92298866690b83b3632992df417
Ao Wang <awang24@gmu.edu>
2019-08-31 22:08:16 -0400

    update reclaim test script

--- reclaim_test/script.go
+++ reclaim_test/script.go
@@ -10,27 +10,36 @@ import (
 	"log"
 	"os"
 	"strconv"
+	"strings"
 	"sync"
+	"sync/atomic"
 	"time"
 )
 
 type lambdaInstance struct {
 	name string
+	//touch        string
+	//firstChange  string
+	//secondChange string
+	timeStamp []string
+	change    bool
 }
 
 var (
+	counter = 0
 	name    = flag.String("name", "reclaim", "lambda function name")
 	num     = flag.Int("count", 1, "lambda Count")
 	m       = flag.Int64("m", 10, "periodic warmup minute")
 	h       = flag.Int64("h", 2, "total time for exp")
 	pre     = flag.String("prefix", "log", "prefix for output log file")
-	LogData = nanolog.AddLogger("%s,%i64")
+	LogData = nanolog.AddLogger("%s")
 	errChan = make(chan error, 1)
 )
 
 func main() {
 	flag.Parse()
 	var wg sync.WaitGroup
+	var sum int32
 	lambdaGroup := make([]*lambdaInstance, *num)
 
 	nanoLogout, err := os.Create(*pre + ".clog")
@@ -51,11 +60,11 @@ func main() {
 
 	for i := range lambdaGroup {
 		wg.Add(1)
-		go lambdaTrigger(lambdaGroup[i], &wg)
+		go lambdaTrigger(lambdaGroup[i], &wg, &sum)
 	}
 	wg.Wait()
 	log.Println("==================")
-	log.Println("interval finished")
+	log.Println("first trigger finished (initial)")
 	log.Println("==================")
 
 	t := time.NewTicker(time.Duration(*m) * time.Minute)
@@ -65,17 +74,26 @@ func main() {
 	for {
 		select {
 		case <-t.C:
+			sum = 0
 			for i := range lambdaGroup {
 				wg.Add(1)
-				go lambdaTrigger(lambdaGroup[i], &wg)
+				go lambdaTrigger(lambdaGroup[i], &wg, &sum)
 			}
 			wg.Wait()
-			log.Println("==================")
-			log.Println("interval finished")
-			log.Println("==================")
+			log.Println("=======================")
+			log.Println(counter, "interval finished", atomic.LoadInt32(&sum), "changed timeStamp")
+			log.Println("=======================")
+			counter = counter + 1
 		case <-t2.C:
-			nanolog.Flush()
+			for i := range lambdaGroup {
+				res := strings.Join(lambdaGroup[i].timeStamp, ",")
+				nanolog.Log(LogData, res)
+			}
 			log.Println("warm up finished")
+			err := nanolog.Flush()
+			if err != nil {
+				fmt.Println("flush err is", err)
+			}
 			return
 		case <-errChan:
 			//nanolog.Flush()
@@ -89,11 +107,13 @@ func main() {
 
 func newLambdaInstance(name string) *lambdaInstance {
 	return &lambdaInstance{
-		name: name,
+		name:      name,
+		timeStamp: make([]string, 1, 500),
+		change:    false,
 	}
 }
 
-func lambdaTrigger(l *lambdaInstance, wg *sync.WaitGroup) {
+func lambdaTrigger(l *lambdaInstance, wg *sync.WaitGroup, s *int32) {
 	sess := session.Must(session.NewSessionWithOptions(session.Options{
 		SharedConfigState: session.SharedConfigEnable,
 	}))
@@ -103,11 +123,30 @@ func lambdaTrigger(l *lambdaInstance, wg *sync.WaitGroup) {
 		fmt.Println("Error calling LambdaFunction", err)
 		errChan <- err
 	}
-	log.Println(l.name, "returned, status code is", *output.StatusCode)
 
 	res := string(output.Payload)
 	res = res[1 : len(res)-1]
 
-	nanolog.Log(LogData, res, *output.StatusCode)
+	//if l.touch == "" {
+	//	l.touch = res
+	//} else {
+	//	if l.touch != res && l.firstChange == "" {
+	//		l.firstChange = res
+	//	}
+	//	if l.touch != res && l.firstChange != res && l.secondChange == "" {
+	//		l.secondChange = res
+	//	}
+	//}
+	if l.timeStamp[0] == "" {
+		l.timeStamp[0] = res
+	} else if res != l.timeStamp[len(l.timeStamp)-1] {
+		l.change = true
+		atomic.AddInt32(s, 1)
+		l.timeStamp = append(l.timeStamp, res)
+	}
+
+	//nanolog.Log(LogData, res, *output.StatusCode)
+	log.Println(l.name, "returned, status code is", *output.StatusCode, "timeStamp changed", l.change)
+	l.change = false
 	wg.Done()
 }
