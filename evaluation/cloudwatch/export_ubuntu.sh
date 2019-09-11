d54fa7c1935ba47c923f539bf354e8c7c63a8c53
Tianium <zhangjyr@gmail.com>
2019-09-10 22:54:59 -0400

    Adjust script

--- evaluation/cloudwatch/export_ubuntu.sh
+++ evaluation/cloudwatch/export_ubuntu.sh
@@ -11,9 +11,19 @@ end=$3
 startTime=$(date -d "$start" +%s)000
 endTime=$(date -d "$end" +%s)000
 
+FROM=0
+TO=399
+if [ "$4" != "" ] ; then
+  FROM=$4
+  TO=$4
+fi
+if [ "$5" != "" ] ; then
+  TO=$5
+fi
 
-for i in {0..399}
+for (( i=$FROM; i<=$TO; i++ ))
 do
+    echo "exporting $LAMBDA$LOG_PREFIX$i"
     aws logs create-export-task --log-group-name $LAMBDA$LOG_PREFIX$i --from ${startTime} --to ${endTime} --destination "tianium.default" --destination-prefix $FILE$PREFIX$LOG_PREFIX$i
     sleep 2s
 done
