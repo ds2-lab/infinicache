e00f4319062b0d946b70548235405096c1e33b89
Ao Wang <awang24@gmu.edu>
2019-09-20 14:11:33 -0400

    update reclaim test script

--- reclaim_test/s3_updatecode.sh
+++ reclaim_test/s3_updatecode.sh
@@ -1,27 +1,19 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-# if [ "$1" != "" ] ; then
-#  PREFIX="$1"
-# fi
-# concurrency=30
+PWD=`dirname $0`
+PREFIX="reclaim"
+KEY="chain"
+cluster=300
+mem=3008
 
 echo "compiling lambda code..."
-GOOS=linux go build reclaim.go
+GOOS=linux go build $KEY.go
 echo "compress file..."
-zip reclaim reclaim
+zip $KEY $KEY
 echo "updating lambda code.."
 
 echo "putting code zip to s3"
-aws s3api put-object --bucket ao.lambda.code --key reclaim.zip --body reclaim.zip
+aws s3api put-object --bucket ao.lambda.code --key $KEY.zip --body $KEY.zip
 
-# for i in {0..299}
-# do
-#     aws lambda update-function-code --function-name $PREFIX$i --s3-bucket ao.lambda.code --s3-key reclaim.zip
-#     # aws lambda update-function-configuration --function-name $PREFIX$i --handler reclaim
-#     # aws lambda update-function-configuration --function-name $PREFIX$i --timeout $2
-# #    aws lambda update-function-configuration --function-name $PREFIX$i --handler redeo_lambda
-# #    aws lambda put-function-concurrency --function-name $PREFIX$i --reserved-concurrent-executions $concurrency
-# done
-
+go run $PWD/../sbin/deploy_function.go -from 0 -to 300 -code=true -config=true -prefix=$PREFIX -vpc=true -key=$KEY -cluster=$cluster -mem=$mem -timeout=$1
 go clean
-rm reclaim.zip
