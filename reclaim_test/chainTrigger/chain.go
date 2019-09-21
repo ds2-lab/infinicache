ec24dd4b0af92eb682ebeaf68a6d7a663dead02e
Ao Wang <awang24@gmu.edu>
2019-09-21 18:38:34 -0400

    add chain lambda handler

--- reclaim_test/chainTrigger/chain.go
+++ reclaim_test/chainTrigger/chain.go
@@ -3,6 +3,7 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"fmt"
 	"github.com/aws/aws-lambda-go/lambda"
 	"github.com/aws/aws-lambda-go/lambdacontext"
@@ -10,37 +11,51 @@ import (
 	"github.com/aws/aws-sdk-go/aws/session"
 	lambdaService "github.com/aws/aws-sdk-go/service/lambda"
 	protocol "github.com/wangaoone/LambdaObjectstore/reclaim_test/types"
-	"os/exec"
-	"strings"
 	"time"
 )
 
 var (
-	t        = time.Now().UnixNano()
-	hostName string
+	t = time.Now().UnixNano()
+	//hostName     string
+	srcCount = 0
+	//replicaCount = 0
 )
 
-func init() {
-	cmd := exec.Command("uname", "-a")
-	host, err := cmd.CombinedOutput()
-	if err != nil {
-		fmt.Println("cmd.Run() failed with ", err)
-	}
-	hostName = strings.Split(string(host), " #")[0]
-}
+//func init() {
+//	cmd := exec.Command("uname", "-a")
+//	host, err := cmd.CombinedOutput()
+//	if err != nil {
+//		fmt.Println("cmd.Run() failed with ", err)
+//	}
+//	hostName = strings.Split(string(host), " #")[0]
+//}
 func HandleRequest(ctx context.Context, input protocol.InputEvent) (string, error) {
-	fmt.Println("deployment is", lambdacontext.FunctionName, "hostName is", hostName, "Global timeStamp is", t)
-	// get first lambda info
-	res := fmt.Sprintf("%s,%s,%d", lambdacontext.FunctionName, hostName, t)
-	if input.Cmd == "trigger" {
-		fmt.Println("going to trigger mySelf")
-		output := trigger(lambdacontext.FunctionName)
-		// get replica lambda info
-		res = fmt.Sprintf("%s,%s", res, output)
-		fmt.Println("me and my replica is", res)
-		return res, nil
+	//// get first lambda info
+	//res := fmt.Sprintf("%s,%s,%d", lambdacontext.FunctionName, hostName, t)
+	//if input.Cmd == "trigger" {
+	//	fmt.Println("going to trigger mySelf")
+	//	output := trigger(lambdacontext.FunctionName)
+	//	// get replica lambda info
+	//	res = fmt.Sprintf("%s,%s", res, output)
+	//	fmt.Println("me and my replica is", res)
+	//	return res, nil
+	//}
+	switch input.Cmd {
+	case "backup":
+		replicaRes := fmt.Sprintf("%d,%s,%d", srcCount, lambdacontext.FunctionName, t)
+		//srcCount = srcCount + 1
+		return replicaRes, nil
+	default:
+		srcRes := fmt.Sprintf("%d,%s,%d", srcCount, lambdacontext.FunctionName, t)
+		if srcCount%5 == 0 && srcCount != 0 {
+			output := trigger(lambdacontext.FunctionName)
+			res := fmt.Sprintf("%s,%s", srcRes, output)
+			srcCount = srcCount + 1
+			return res, nil
+		}
+		srcCount = srcCount + 1
+		return srcRes, nil
 	}
-	return res, nil
 }
 
 func main() {
@@ -52,13 +67,13 @@ func trigger(name string) string {
 		SharedConfigState: session.SharedConfigEnable,
 	}))
 	client := lambdaService.New(sess, &aws.Config{Region: aws.String("us-east-1")})
-	//event := &protocol.InputEvent{
-	//	Cmd: "",
-	//}
-	//payload, _ := json.Marshal(event)
+	event := &protocol.InputEvent{
+		Cmd: "backup",
+	}
+	payload, _ := json.Marshal(event)
 	input := &lambdaService.InvokeInput{
 		FunctionName: aws.String(name),
-		//Payload:      payload,
+		Payload:      payload,
 	}
 	output, err := client.Invoke(input)
 	if err != nil {
