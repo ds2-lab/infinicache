1d37454b68fa64e6d12697f9bcdd00d153abc521
Tianium <zhangjyr@gmail.com>
2019-12-22 05:02:25 -0500

    Enhance script

--- evaluation/cloudwatch/export.sh
+++ evaluation/cloudwatch/export.sh
@@ -12,6 +12,24 @@ endTime=$(date  -j -f "%Y-%m-%d %H:%M:%S" "$end" +%s)000
 
 for i in {0..5}
 do
-    aws logs create-export-task --log-group-name $LAMBDA$i --from ${startTime} --to ${endTime} --destination "ao.cost.log" --destination-prefix $FILE$i
-    sleep 2s
+  # Wait for the end the last task
+  for j in {0..15}
+  do
+    RUNNING=`aws logs describe-export-tasks --status-code "RUNNING" | grep taskId | awk -F \" '{ print $4 }'`
+    if [ "$RUNNING" != "" ]; then
+      sleep 2s
+    else
+      break
+    fi
+  done
+
+  # Abandon
+  if [ "$RUNNING" != "" ]; then
+    echo "Detect running task and wait timeout, killing task \"$RUNNING\"..."
+    aws logs --profile CWLExportUser cancel-export-task --task-id \"$RUNNING\"
+    echo "Done"
+  fi
+
+  aws logs create-export-task --log-group-name $LAMBDA$i --from ${startTime} --to ${endTime} --destination "ao.cost.log" --destination-prefix $FILE$i
+  sleep 2s
 done
