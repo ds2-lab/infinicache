cd50294469387a0a7a0d8f11b8ef3b9d17ceacb9
Ao Wang <awang24@gmu.edu>
2019-12-26 17:19:45 -0500

    update script

--- evaluation/cloudwatch/parse.sh
+++ evaluation/cloudwatch/parse.sh
@@ -20,9 +20,11 @@ for file in $PWD/$LOGSET/*
 do
 	for zip in ${file}/*/*/*
     do
-		cat ${zip} >> $LOGSET.dat
+#		cat ${zip} >> $LOGSET.dat
+		cat ${zip} | grep -E 'START|New lambda invocation|REPORT' | sed '/START/{ s/^.*$/1 2 3 4 5 invocation:unknown/;h;d;};/invocation/{s/\(invocation:\) \(.*\)/\1\2/;h;d;};/REPORT/{H;g;s/\n/ /;}' | awk '{print $7","$6","$10","$12","$16}' >> $PWD/${LOGSET}_bill.csv
+
 	done
 done
 
-echo "cat $PWD/$LOGSET.dat | grep -E 'START|New lambda invocation|REPORT' | sed '/START/{ s/^.*$/1 2 3 4 5 invocation:unknown/;h;d;};/invocation/{s/\(invocation:\) \(.*\)/\1\2/;h;d;};/REPORT/{H;g;s/\n/ /;}' | awk '{print $7","$6","$10","$12","$16}' > $PWD/${LOGSET}_bill.csv"
-cat $PWD/$LOGSET.dat | grep -E 'START|New lambda invocation|REPORT' | sed '/START/{ s/^.*$/1 2 3 4 5 invocation:unknown/;h;d;};/invocation/{s/\(invocation:\) \(.*\)/\1\2/;h;d;};/REPORT/{H;g;s/\n/ /;}' | awk '{print $7","$6","$10","$12","$16}' > $PWD/${LOGSET}_bill.csv
+#echo "cat $PWD/$LOGSET.dat | grep -E 'START|New lambda invocation|REPORT' | sed '/START/{ s/^.*$/1 2 3 4 5 invocation:unknown/;h;d;};/invocation/{s/\(invocation:\) \(.*\)/\1\2/;h;d;};/REPORT/{H;g;s/\n/ /;}' | awk '{print $7","$6","$10","$12","$16}' > $PWD/${LOGSET}_bill.csv"
+#cat $PWD/$LOGSET.dat | grep -E 'START|New lambda invocation|REPORT' | sed '/START/{ s/^.*$/1 2 3 4 5 invocation:unknown/;h;d;};/invocation/{s/\(invocation:\) \(.*\)/\1\2/;h;d;};/REPORT/{H;g;s/\n/ /;}' | awk '{print $7","$6","$10","$12","$16}' > $PWD/${LOGSET}_bill.csv
