bc5570aa8097d631e5d120b80d1c8e7bc32c1779
nepotu <neboduus@gmail.com>
2020-06-29 18:22:54 +0200

    change i in GET data iteration

--- proxy/proxy/server/proxy.go
+++ proxy/proxy/server/proxy.go
@@ -121,7 +121,7 @@ func (p *Proxy) HandleMkSet(w resp.ResponseWriter, c *resp.Command) {
 		fmt.Println("Arg", i, " - ", c.Arg(i).String())
 	}*/
 
-	for i := 9; i < c.ArgN(); i=i+2 {
+	for i := 8; i < c.ArgN(); i=i+2 {
 		lowLevelKey := c.Arg(i).String()
 		lowLevelKeys = append(lowLevelKeys, lowLevelKey)
 		value := c.Arg(i+1).Bytes()
@@ -291,7 +291,7 @@ func (p *Proxy) HandleMkGet(w resp.ResponseWriter, c *resp.Command) {
 	lowLevelKeysN, _ := c.Arg(5).Int()
 	lowLevelKeys := make([]string, lowLevelKeysN)
 
-	for i:=7; i<c.ArgN() ;i++{
+	for i:=6; i<c.ArgN() ;i++{
 		lowLevelKey := c.Arg(i).String()
 		lowLevelKeys = append(lowLevelKeys, lowLevelKey)
 	}
