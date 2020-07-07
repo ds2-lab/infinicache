c7661579c138db5d9224987c711d3c8c75fe108c
nepotu <neboduus@gmail.com>
2020-07-07 16:10:17 +0200

    adjusts ip

--- proxy/proxy/server/config.go
+++ proxy/proxy/server/config.go
@@ -29,27 +29,27 @@ var LambdaAddresses = [...]string {
 	"http://infinicache-node-17.default.34.91.116.154.xip.io",
 	"http://infinicache-node-18.default.34.91.116.154.xip.io",
 	"http://infinicache-node-19.default.34.91.116.154.xip.io",*/
-	//"http://infinicache-node-0.default.svc.cluster.local",
-	//"http://infinicache-node-1.default.svc.cluster.local",
-	//"http://infinicache-node-2.default.svc.cluster.local",
-	//"http://infinicache-node-3.default.svc.cluster.local",
-	//"http://infinicache-node-4.default.svc.cluster.local",
-	//"http://infinicache-node-5.default.svc.cluster.local",
-	//"http://infinicache-node-6.default.svc.cluster.local",
-	//"http://infinicache-node-7.default.svc.cluster.local",
-	//"http://infinicache-node-8.default.svc.cluster.local",
-	//"http://infinicache-node-9.default.svc.cluster.local",
-	//"http://infinicache-node-10.default.svc.cluster.local",
-	//"http://infinicache-node-11.default.svc.cluster.local",
-	//"http://infinicache-node-12.default.svc.cluster.local",
-	//"http://infinicache-node-13.default.svc.cluster.local",
-	//"http://infinicache-node-14.default.svc.cluster.local",
-	//"http://infinicache-node-15.default.svc.cluster.local",
-	//"http://infinicache-node-16.default.svc.cluster.local",
-	//"http://infinicache-node-17.default.svc.cluster.local",
-	//"http://infinicache-node-18.default.svc.cluster.local",
-	//"http://infinicache-node-19.default.svc.cluster.local",
-"http://infinicache-node-20.default.svc.cluster.local",
+	"http://infinicache-node-0.default.svc.cluster.local",
+	"http://infinicache-node-1.default.svc.cluster.local",
+	"http://infinicache-node-2.default.svc.cluster.local",
+	"http://infinicache-node-3.default.svc.cluster.local",
+	"http://infinicache-node-4.default.svc.cluster.local",
+	"http://infinicache-node-5.default.svc.cluster.local",
+	"http://infinicache-node-6.default.svc.cluster.local",
+	"http://infinicache-node-7.default.svc.cluster.local",
+	"http://infinicache-node-8.default.svc.cluster.local",
+	"http://infinicache-node-9.default.svc.cluster.local",
+	"http://infinicache-node-10.default.svc.cluster.local",
+	"http://infinicache-node-11.default.svc.cluster.local",
+	"http://infinicache-node-12.default.svc.cluster.local",
+	"http://infinicache-node-13.default.svc.cluster.local",
+	"http://infinicache-node-14.default.svc.cluster.local",
+	"http://infinicache-node-15.default.svc.cluster.local",
+	"http://infinicache-node-16.default.svc.cluster.local",
+	"http://infinicache-node-17.default.svc.cluster.local",
+	"http://infinicache-node-18.default.svc.cluster.local",
+	"http://infinicache-node-19.default.svc.cluster.local",
+/*"http://infinicache-node-20.default.svc.cluster.local",
 	"http://infinicache-node-21.default.svc.cluster.local",
 	"http://infinicache-node-22.default.svc.cluster.local",
 	"http://infinicache-node-23.default.svc.cluster.local",
@@ -68,15 +68,15 @@ var LambdaAddresses = [...]string {
 	"http://infinicache-node-36.default.svc.cluster.local",
 	"http://infinicache-node-37.default.svc.cluster.local",
 	"http://infinicache-node-38.default.svc.cluster.local",
-	"http://infinicache-node-39.default.svc.cluster.local",
+	"http://infinicache-node-39.default.svc.cluster.local",*/
 }
 const LambdaStoreName = "LambdaStore" // replica version (no use)
 const LambdaPrefix = "CacheNode"
 const InstanceWarmTimout = 1 * time.Minute
 const InstanceCapacity = 1536 * 1000000 // MB
 const InstanceOverhead = 100 * 1000000  // MB
-//const ServerPublicIp = "10.4.0.101" // Leave it empty if using VPC.
- const ServerPublicIp = "10.4.14.71"
+const ServerPublicIp = "10.4.0.101" // Leave it empty if using VPC.
+//const ServerPublicIp = "10.4.14.71"
 
 func init() {
 	lambdastore.WarmTimout = InstanceWarmTimout
