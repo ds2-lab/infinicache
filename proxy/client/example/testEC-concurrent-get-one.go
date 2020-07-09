d2084527795e0233482fa4fb9fb85d8e6083acfd
nepotu <neboduus@gmail.com>
2020-07-09 22:35:14 +0200

    configure each client to set and get different keys in concurrent experiment

---
+++ proxy/client/example/testEC-concurrent-get-one.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"github.com/neboduus/infinicache/proxy/client"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	requestsNumber, size, addrList := client.GetArgs(os.Args)
+	var wg sync.WaitGroup
+	for i:=0; i<3; i++{
+		wg.Add(1)
+		go test5_1(i, &wg, addrList, requestsNumber, size)
+	}
+	wg.Wait()
+}
+
+func test5_1(i int, wg *sync.WaitGroup, addrList string, reqNumber int, size int){
+	defer wg.Done()
+
+	// parse server address
+	addrArr := strings.Split(addrList, ",")
+
+	// initial new ecRedis client
+	cli := client.NewClient(10, 2, 32, 3)
+
+	// start dial and PUT/GET
+	cli.Dial(addrArr)
+	val := make([]byte, size)
+	rand.Read(val)
+	// ToDo: Replace with a channel
+	var getStats []float64
+
+	for k:=0; k<reqNumber; k++{
+		key := fmt.Sprintf("k.%d.%d", i, k)
+
+		var s float64 = 0
+		for l:=0; l<1; l++ {
+			key := fmt.Sprintf("%s.%d", key, l)
+			if _, _, stats, ok := cli.EcGet(key, size); !ok {
+				log.Println("Failed to GET ", key)
+			} else {
+				log.Println("Successfull GET ", key)
+				getStats = append(getStats, stats)
+			}
+		}
+		if s!=0{
+			getStats = append(getStats, s)
+		}
+	}
+
+	gMin, gMax, gAvg, gSd, gPercentiles := cli.GetStats(getStats)
+	log.Println("SET stats ", gMin, gMax, gAvg, gSd, gPercentiles)
+	return
+}
---
+++ proxy/client/example/testEC-concurrent-set-one.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"math/rand"
+	"fmt"
+	"github.com/neboduus/infinicache/proxy/client"
+	"log"
+	"os"
+	"strings"
+	"sync"
+)
+
+func main() {
+	requestsNumber, size, addrList := client.GetArgs(os.Args)
+	var wg sync.WaitGroup
+	for i:=0; i<3; i++{
+		wg.Add(1)
+		go test6_1(i, &wg, addrList, requestsNumber, size)
+	}
+	wg.Wait()
+}
+
+func test6_1(i int, wg *sync.WaitGroup, addrList string, reqNumber int, size int){
+	defer wg.Done()
+
+	// parse server address
+	addrArr := strings.Split(addrList, ",")
+
+	// initial new ecRedis client
+	cli := client.NewClient(10, 2, 32, 3)
+
+	// start dial and PUT/GET
+	cli.Dial(addrArr)
+	val := make([]byte, size)
+	rand.Read(val)
+	// ToDo: Replace with a channel
+	var setStats []float64
+
+	for k:=0; k<reqNumber; k++{
+		key := fmt.Sprintf("k.%d.%d", i, k)
+
+		var s float64 = 0
+		for l:=0; l<1; l++ {
+			key := fmt.Sprintf("%s.%d", key, l)
+			if _, stats, ok := cli.EcSet(key, val); !ok {
+				log.Println("Failed to SET ", key)
+			}else{
+				log.Println("Successfull SET ", key)
+				s += stats
+			}
+		}
+		if s!=0{
+			setStats = append(setStats, s)
+		}
+	}
+
+	gMin, gMax, gAvg, gSd, gPercentiles := cli.GetStats(setStats)
+	log.Println("SET stats ", gMin, gMax, gAvg, gSd, gPercentiles)
+	return
+}
