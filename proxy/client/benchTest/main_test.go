db748f8754f9537d94d95c69f3e9969cf4b31b00
nepotu <neboduus@gmail.com>
2020-07-13 15:23:35 +0200

    adds v1 benchmark test

--- proxy/client/benchTest/main_test.go
+++ proxy/client/benchTest/main_test.go
@@ -6,16 +6,30 @@ import (
 	"testing"
 )
 
-
 func BenchmarkMkSet(b *testing.B) {
 	sizes := []int{1,160,500,1600}
 	cli := client.NewClient(10, 2, 32, 3)
 	cli.Dial([]string{"10.4.0.100:6378"})
 	for size := range sizes {
 		data := cli.GenerateSetData(size)
-		b.Run(fmt.Sprintf("%s/%d", "MK_SET", size), func(b *testing.B) {
+		b.Run(fmt.Sprintf("/%d B", size), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
-				_,_,_ = cli.MkSet("testK", data)
+				_,_,_ = cli.MkSet(fmt.Sprintf("k%d", i), data)
+			}
+		})
+	}
+}
+
+func BenchmarkMkGet(b *testing.B) {
+	sizes := []int{1,160,500,1600}
+	cli := client.NewClient(10, 2, 32, 3)
+	cli.Dial([]string{"10.4.0.100:6378"})
+	for size := range sizes {
+		d_set := cli.GenerateSetData(size)
+		d_get := cli.GenerateSingleRandomGet(d_set)
+		b.Run(fmt.Sprintf("/%d B", size), func(b *testing.B) {
+			for i := 0; i < b.N; i++ {
+				_,_,_ = cli.MkGet(fmt.Sprintf("k-%d-%d", i, size), d_get)
 			}
 		})
 	}
