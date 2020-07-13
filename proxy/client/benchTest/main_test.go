9df1a22b3eb020d048842175579b178f55e93f0f
nepotu <neboduus@gmail.com>
2020-07-13 15:47:38 +0200

    adds v1 benchmark test

--- proxy/client/benchTest/main_test.go
+++ proxy/client/benchTest/main_test.go
@@ -11,7 +11,7 @@ func BenchmarkMkSet(b *testing.B) {
 	sizes := []int{1,160,500,1600}
 	cli := client.NewClient(10, 2, 32, 3)
 	cli.Dial([]string{"10.4.0.100:6378"})
-	for size := range sizes {
+	for _, size := range sizes {
 		data := cli.GenerateSetData(size)
 		b.Run(fmt.Sprintf("/%d B", size), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
@@ -29,7 +29,7 @@ func BenchmarkMkGet(b *testing.B) {
 	// we first set some data to be sure our GET ops are successfull
 	allSets := make(map[int][][3]client.KVSetGroup)
 	var allKeys []string
-	for size := range sizes {
+	for _, size := range sizes {
 		dSet := cli.GenerateSetData(size)
 		var okSets [][3]client.KVSetGroup
 		for i := 0; i <= 500; i++{
@@ -43,7 +43,7 @@ func BenchmarkMkGet(b *testing.B) {
 		allSets[size] = okSets
 	}
 
-	for size := range sizes {
+	for _, size := range sizes {
 		setOps := allSets[size]
 		b.Run(fmt.Sprintf("/%d B", size), func(b *testing.B) {
 			for i := 0; i < b.N; i++ {
