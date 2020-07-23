1f984bc039907f5e89919af255e28d3167097f32
nepotu <neboduus@gmail.com>
2020-07-23 16:58:17 +0200

    adds first redis test

--- proxy/tmp/redis.go
+++ proxy/tmp/redis.go
@@ -18,6 +18,7 @@ package main
 import (
 	"fmt"
 	"github.com/gistao/RedisGo-Async/redis"
+	"github.com/montanaflynn/stats"
 	"log"
 	"math/rand"
 	"sync"
@@ -323,7 +324,7 @@ func main() {
 	p = generateInput(0,9, size)
 	rdc.MkGet(p)
 
-	/*var s []float64
+	var s []float64
 	for i:=0;i<10000;i++{
 		input := generateInput(i,9, size)
 		t := time.Now()
@@ -365,5 +366,5 @@ func main() {
 	}else{
 		log.Println("Get Mean Err: ", err)
 		return
-	}*/
+	}
 }
