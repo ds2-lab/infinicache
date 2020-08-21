3b8e7f94c84a6d3f072ddefc3839134ac7d55de7
nepotu <neboduus@gmail.com>
2020-08-21 10:37:23 +0200

    adds cumulative tetst

---
+++ proxy/client/test/testR-get-cumulative.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"github.com/neboduus/infinicache/proxy/client"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+	"math/rand"
+)
+
+func main() {
+	requestsNumber, size, addrList := client.GetArgs(os.Args)
+	val := make([]byte, size)
+	rand.Read(val)
+
+	// parse server address
+	addrArr := strings.Split(addrList, ",")
+
+	// initial new ecRedis client
+	cli := client.NewClient(10, 2, 32, 3)
+
+	// start dial and PUT/GET
+	cli.Dial(addrArr)
+	var getStats []float64
+	requestsNumber, err := strconv.Atoi(os.Args[1])
+	if err!=nil{
+		log.Fatal("No arguments for test. requests number expected")
+	}
+
+	for k:=0; k<=requestsNumber; k++{
+		key := fmt.Sprintf("k.%d", k)
+
+		var s float64 = 0
+		for l:=0; l<9; l++ {
+			if _, reader, stats, ok := cli.RGet(key, len(val)); !ok {
+				log.Println("Failed to get ", key)
+				return
+			} else {
+				buf := new(bytes.Buffer)
+				buf.ReadFrom(reader)
+				reader.Close()
+				//s := buf.String()
+				log.Println("Successfull rGET", key)
+				s += stats
+			}
+		}
+		if s!=0{
+			getStats = append(getStats, s)
+		}
+
+	}
+
+	gMin, gMax, gAvg, gSd, gPercentiles := cli.GetStats(getStats)
+	log.Println("GET stats ", gMin, gMax, gAvg, gSd, gPercentiles)
+	return
+}
+
---
+++ proxy/client/test/testR-set-cumulative.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"github.com/neboduus/infinicache/proxy/client"
+	"log"
+	"math/rand"
+	"os"
+	"strings"
+)
+
+func main() {
+	requestsNumber, size, addrList :=client.GetArgs(os.Args)
+	val := make([]byte, size)
+	rand.Read(val)
+
+	// parse server address
+	addrArr := strings.Split(addrList, ",")
+
+	// initial new ecRedis client
+	cli := client.NewClient(10, 2, 32, 3)
+
+	// start dial and PUT/GET
+	cli.Dial(addrArr)
+	var setStats []float64
+
+	for k:=0; k<=requestsNumber; k++{
+		key := fmt.Sprintf("k.%d", k)
+
+		var s float64 = 0
+		for l:=0; l<9; l++ {
+			if _, stats, ok := cli.RSet(key, val); !ok {
+				log.Println("Failed to SET ", key)
+			}else{
+				log.Println("Successfull SET ", key)
+				s += stats
+			}
+		}
+		if s!=0{
+			setStats = append(setStats, s)
+		}
+
+	}
+
+	sMin, sMax, sAvg, sSd, sPercentiles := cli.GetStats(setStats)
+	log.Println("SET stats ", sMin, sMax, sAvg, sSd, sPercentiles)
+
+}
+
